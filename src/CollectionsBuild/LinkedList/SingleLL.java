4dfb4f37d46a5b39703419d66c80c486f28e2baf
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-12 11:45:55 +0530

    changes all-1.0

--- src/CollectionsBuild/LinkedList/SingleLL.java
+++ src/CollectionsBuild/LinkedList/SingleLL.java
@@ -4,7 +4,7 @@ class Node{
     int val;
     Node next;
     public Node(int val){
-        this.val=val;
+        this.val=val; //s
         //System.out.println(this.val+" : "+this.next); // Checkk
     }
 }
