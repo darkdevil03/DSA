8d18d7016e5972bcb5d3096f388de09ad174206e
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-18 14:07:59 +0530

    Daily Question LeetCode 796 Solution has added.

---
+++ src/LeetcodeProb/LeetCode796.java
@@ -0,0 +1,40 @@
+package LeetcodeProb;
+
+/*
+Daily Question
+    LeetCode 796 : https://leetcode.com/problems/rotate-string?envType=daily-question&envId=2026-05-03
+*/
+
+public class LeetCode796 {
+    static boolean rotateString(String s, String goal) {
+
+        if(s.length() != goal.length()){
+            return false;
+        }
+
+        int sLen = s.length();
+        int goalLen = goal.length();
+
+        int nonMatchIndexOf_S = 0;
+
+        // substring() : https://www.w3schools.com/java/ref_string_substring.asp
+
+        for(int i = 1; i <= sLen; i++){
+            if( (s.substring(nonMatchIndexOf_S , i) ).equals(goal.substring(goalLen - i , goalLen)) ){
+                nonMatchIndexOf_S = i;
+            }
+        }
+
+        return ( (s.substring(nonMatchIndexOf_S) ).equals(goal.substring(0, (goalLen-nonMatchIndexOf_S))) ) ;
+
+    }
+
+    public static void main(String[] args) {
+
+        // Taken one public case here
+        String given_s= "abcde";
+        String given_goal= "cdeab";
+
+        System.out.println(rotateString(given_s,given_goal) );
+    }
+}
