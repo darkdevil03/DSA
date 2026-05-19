e64a2be972cb1fe83f59908f7cf73bff1faa9843
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-19 13:27:33 +0530

    LeetCode 33 solution has added.

---
+++ src/LeetcodeProb/LeetCode33.java
@@ -0,0 +1,54 @@
+package LeetcodeProb;
+
+/*
+    https://leetcode.com/problems/search-in-rotated-sorted-array?envType=study-plan-v2&envId=binary-search
+*/
+
+public class LeetCode33 {
+    static int search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+
+        while ( start <= end ){
+
+            int mid = start + ((end - start)/2);
+
+            if(target == nums[mid]){
+                return mid;
+            }
+            // Step 1: Check if the LEFT half is sorted
+            if (nums[start] <= nums[mid]) {
+                // Step 2: Check if target lies within this sorted LEFT half
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1; // Narrow down to the left
+                } else {
+                    start = mid + 1; // Otherwise, look in the right half
+                }
+            }
+            // Step 1 Check: If the left isn't sorted, the RIGHT half must be sorted
+            else {
+                // Step 2: Check if target lies within this sorted RIGHT half
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1; // Narrow down to the right
+                } else {
+                    end = mid - 1; // Otherwise, look in the left half
+                }
+            }
+
+        }
+
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        // Taken one public case here
+        int[] nums = {6,7,0,1,2,3,4,5};
+
+        System.out.println(search(nums,0) );
+    }
+}
