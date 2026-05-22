45152638d4ca6c234e7054e5a8711d498229e253
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-22 23:43:54 +0530

    LeetCode367 Solution has added.

---
+++ src/LeetcodeProb/BinarySearch/LeetCode367.java
@@ -0,0 +1,37 @@
+package LeetcodeProb.BinarySearch;
+
+/*
+    https://leetcode.com/problems/valid-perfect-square?envType=study-plan-v2&envId=binary-search
+*/
+
+public class LeetCode367 {
+
+    public boolean isPerfectSquare(int num) {
+
+        if(num == 1)
+            return true;
+
+        long start = 2;
+        long end = num;
+
+        while(start <= (end)){
+            long mid = start + ((end -start)/2);
+
+            if( mid * mid == num){
+                return true;
+            }else if(mid * mid < num){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int num = 16;
+        LeetCode367 leetCode367 = new LeetCode367();
+        System.out.println(leetCode367.isPerfectSquare(num));
+    }
+}
