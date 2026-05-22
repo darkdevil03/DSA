e7805413524c6369934630875f96cf2e48f241c9
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-22 23:30:16 +0530

    LeetCode69 Solution has added.

---
+++ src/LeetcodeProb/BinarySearch/LeetCode69.java
@@ -0,0 +1,44 @@
+package LeetcodeProb.BinarySearch;
+
+/*
+    https://leetcode.com/problems/sqrtx?envType=study-plan-v2&envId=binary-search
+*/
+
+public class LeetCode69 {
+
+        static int mySqrt(int x) {
+
+            if(x == 0 || x == 1){
+                return x;
+            }
+
+            int start = 1;
+            int end = x;
+            int ans = 0;
+
+            while(start <= end){
+
+                int mid = start + ((end - start)/2);
+
+                if((x/mid) == mid){
+                    return  mid;
+                }else if((x/mid) > mid ){
+                    ans = mid;
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+
+                }
+
+            }
+
+            return ans;
+
+        }
+
+        public static void main(String[] args) {
+            int num = 8;
+            System.out.println(mySqrt(num));
+        }
+
+}
