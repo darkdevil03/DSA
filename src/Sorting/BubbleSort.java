0973b87169faefc11243c59f243563d13ffa32d0
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-26 22:05:44 +0530

    For BubbleSort Main method with asc and des order input has added.

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,10 +1,8 @@
 package Sorting;
 
-import Sorting.Swap;
-
 public class BubbleSort {
 
-    Swap s=new Swap();  // method: swap(arr,index1,index2)
+    Swap s=new Swap();  // method: swap(arr,index1,index2) from import Sorting.Swap; package
 
     //Ascending Order
     public int[] BubbleSortAsc(int[] arr){
@@ -18,15 +16,47 @@ public class BubbleSort {
     }
 
     //Descending order
-    public int[] BubbleSortDsc(int[] arr){
+    public int[] BubbleSortDes(int[] arr){
         int len=arr.length-1;
-        System.out.println();
         for(int i=0;i<len;i++){
             for(int j=0;j<len-i;j++)
                 if(arr[j]<arr[j+1])
                     s.swap(arr,j,j+1);
         }
         return arr;
+    }
+
+    static void main(){
+
+        int[] arrForAsc = {8, 6, 7, 1, 4, 2};
+        int[] arrForDes = {8, 6, 7, 1, 4, 2};
+
+    // Bubble Sort
+
+        System.out.print("Before Bubble Sorting : ");
+        for (int nums : arrForAsc) {
+            System.out.print(nums + " ");
+        }
+
+        System.out.println();
+
+        //Bubble Sort Ascending Order
+        System.out.print("Bubble Sorting Ascending Order : ");
+        BubbleSort bubbleAsc = new BubbleSort();
+        int[] resultAsc = bubbleAsc.BubbleSortAsc(arrForAsc);
+        for (int nums : resultAsc) {
+            System.out.print(nums + " ");
+        }
+
+        System.out.println();
+
+        //Bubble Sort Descending Order
+        System.out.print("Bubble Sorting Descending Order : ");
+        BubbleSort bubbleDsc = new BubbleSort();
+        int[] resultDsc = bubbleDsc.BubbleSortDes(arrForDes);
+        for(int nums :resultDsc) {
+            System.out.print(nums + " ");
+        }
     }
 
 }
