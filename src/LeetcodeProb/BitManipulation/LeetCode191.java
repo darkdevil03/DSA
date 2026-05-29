c8b60b70617f817bce5253f8ffdb2d3042586541
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-29 08:05:46 +0530

    LeetCode191 solution has added.

---
+++ src/LeetcodeProb/BitManipulation/LeetCode191.java
@@ -0,0 +1,39 @@
+package LeetcodeProb.BitManipulation;
+
+/*
+    https://leetcode.com/problems/number-of-1-bits?envType=problem-list-v2&envId=bit-manipulation
+*/
+
+public class LeetCode191 {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while(n > 0){
+            if((n & 1) == 1)
+                count++;
+
+            n = n >> 1;
+        }
+
+        return count;
+    }
+
+    // The Ultimate Optimization (Brian Kernighan's)
+    public int hammingWeightApproach2(int n) {
+        int count = 0;
+        while(n != 0){
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+
+    static void main(){
+        LeetCode191 leetCode191 = new LeetCode191();
+        int num = 128;
+
+        System.out.println(leetCode191.hammingWeight(num));
+
+        System.out.println(leetCode191.hammingWeightApproach2(num));
+    }
+}
