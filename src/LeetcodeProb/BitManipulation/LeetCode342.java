c8e3920ffa75c3a03ebef18f9c0cbba621ff6e9a
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-05-30 23:37:59 +0530

    LeetCode342 added solution using loops.

---
+++ src/LeetcodeProb/BitManipulation/LeetCode342.java
@@ -0,0 +1,34 @@
+package LeetcodeProb.BitManipulation;
+
+/*
+    https://leetcode.com/problems/power-of-four?envType=problem-list-v2&envId=bit-manipulation
+*/
+
+public class LeetCode342 {
+    public boolean isPowerOfFour(int n) {
+        if(n == 1){
+            return true;
+        }
+
+        while(n > 1){
+
+            if(n == 4){
+                return true;
+            }
+
+            n = n >> 2;
+        }
+
+        return false;
+
+    }
+
+    static void main() {
+
+        int num = 16;
+
+        LeetCode342 leetCode342 = new LeetCode342();
+
+        System.out.println(leetCode342.isPowerOfFour(num));
+    }
+}
