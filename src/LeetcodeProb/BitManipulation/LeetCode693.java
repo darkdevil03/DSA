6acc869940ef8de815005ccbeffb4c8d4cc15d97
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-06-07 23:23:01 +0530

    For LeetCode693 solution has added.

---
+++ src/LeetcodeProb/BitManipulation/LeetCode693.java
@@ -0,0 +1,26 @@
+package LeetcodeProb.BitManipulation;
+
+/*
+    https://leetcode.com/problems/binary-number-with-alternating-bits?envType=problem-list-v2&envId=bit-manipulation
+*/
+
+public class LeetCode693 {
+    public boolean hasAlternatingBits(int n) {
+        int adjacent = n & 1;
+        n = n >> 1;
+
+        while(n > 0){
+            if((n & 1) == adjacent)
+                return false;
+            adjacent = n & 1;
+            n = n >> 1;
+        }
+
+        return true;
+    }
+
+    static void main(){
+        LeetCode693 leetcode693 = new LeetCode693();
+        System.out.println(leetcode693.hasAlternatingBits(5));
+    }
+}
