d99cb2759a9db59142ca0157a3da783f38bca637
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-06-10 00:11:39 +0530

    For LeetCode9 added the time and space complexity of both approaches.

--- src/LeetcodeProb/Math/LeetCode9.java
+++ src/LeetcodeProb/Math/LeetCode9.java
@@ -2,30 +2,49 @@ package LeetcodeProb.Math;
 
 /*
     https://leetcode.com/problems/palindrome-number?envType=problem-list-v2&envId=math
+
+    Approach-1 : Using string conversion
+
+        Time Complexity  : O(n) where n is the number of digits.
+
+        Space Complexity : O(n) auxiliary space.
+
+    Approach-2 : Using math
+
+        Time Complexity  : O(log(n)) where n is the value of the number.
+
+        Space Complexity : O(1) auxiliary space (Constant Space).
+
 */
 public class LeetCode9 {
 
-    // Converts the integer to a String :
+    // Approach-1 : Converts the integer to a String :
     public boolean isPalindrome1(int x) {
 
-        String str = ""+x;
+        String str = ""+x;  // extra space
 
         int j = str.length() ;
 
-        for(int i = 0 ;i <= ( j / 2 ); i++){
+        for(int i = 0 ;i <= ( j / 2 ); i++){  // loop over half-string (n/2).
             if(str.charAt(i) !=  str.charAt(j - i - 1))
                 return false;
         }
+        /*
+            Notice the relationship: when the number of digits (n) doubles, the number of loop iterations also doubles.
+            Because the growth rate scales perfectly linearly with n, mathematically we write it as:
+            O(n/2) => ((1/2) * n) => O(n). ; (implies :  =>)
+         */
 
         return true;
+
     }
 
-    // Optimized approach using Math operations :
+    // Approach-2 : Optimized approach using Math operations :
     public boolean isPalindrome2(int x){
-        // Special cases:
-        // As discussed, negative numbers are not palindromes (e.g., -121)
-        // Also, if the last digit of the number is 0, for it to be a palindrome,
-        // the first digit also needs to be 0. Only 0 itself satisfies this.
+        /* Special cases:
+           As discussed, negative numbers are not palindromes (e.g., -121)
+           Also, if the last digit of the number is 0, for it to be a palindrome,
+           the first digit also needs to be 0. Only 0 itself satisfies this. */
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
@@ -37,9 +56,9 @@ public class LeetCode9 {
             x /= 10;
         }
 
-        // When the length is an odd number, we can get rid of the middle digit by reversedHalf/10
-        // For example, when x = 12321, at the end of the while loop we get x = 12, reversedHalf = 123
-        // Since the middle digit doesn't matter in palindrome, we can safely divide it by 10.
+        /* When the length is an odd number, we can get rid of the middle digit by reversedHalf/10
+           For example, when x = 12321, at the end of the while loop we get x = 12, reversedHalf = 123
+           Since the middle digit doesn't matter in palindrome, we can safely divide it by 10. */
         return x == reversedHalf || x == reversedHalf / 10;
 
     }
@@ -47,8 +66,9 @@ public class LeetCode9 {
     static void main(){
         LeetCode9 leetCode9 = new LeetCode9();
 
-        System.out.println(leetCode9.isPalindrome1(-121));
+        System.out.println("Using approach-1 : "+leetCode9.isPalindrome1(-121));
 
-        System.out.println(leetCode9.isPalindrome2(-121));
+        System.out.println("Using approach-2 : "+leetCode9.isPalindrome2(-121));
+
     }
 }
