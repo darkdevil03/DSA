a6a0eb443831815c617d3666b73af596de0f1377
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-06-11 19:28:45 +0530

    LeetCode29 solution added.

---
+++ src/LeetcodeProb/Math/LeetCode29.java
@@ -0,0 +1,22 @@
+package LeetcodeProb.Math;
+
+/*
+    https://leetcode.com/problems/divide-two-integers?envType=problem-list-v2&envId=math
+*/
+
+public class LeetCode29 {
+    public int divide(int dividend, int divisor) {
+
+        // Return 2147483647 to handle the overflow
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        return (dividend/divisor);
+    }
+
+    static void main(){
+        LeetCode29 leetCode29 = new LeetCode29();
+        System.out.println(leetCode29.divide(10, 10));
+    }
+}
