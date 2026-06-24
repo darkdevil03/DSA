a4b33b5ed85a8b5dfb4ea3cb434e8dc195f933bd
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-06-24 20:41:58 +0530

    Added Example Question for DefaultConstructor.java.

--- src/JavaOOPS/DefaultConstructor.java
+++ src/JavaOOPS/DefaultConstructor.java
@@ -1,15 +1,24 @@
 package JavaOOPS;
 
+/*
+    Question : Write a Java program to create a class called "Cat" with instance variables name and age.
+               Implement a default constructor that initializes the name to "Unknown" and the age to 0.
+               Print the values of the variables.
+ */
+
 class Cat{
 
+    // Instance variables
     String name;
     int age;
 
+    // Default constructor
     Cat(){
         name = "Unknown";
         age = 0;
     }
 
+    // Print the values
     void printCat(){
         System.out.println(name);
         System.out.println(age);
