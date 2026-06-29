cae01d3c5a507d996d57839d56c69cc9962d947a
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-06-29 22:41:39 +0530

    For LeetCode58, approach-2 added.

--- src/leetcode_problems/strings/LeetCode58.java
+++ src/leetcode_problems/strings/LeetCode58.java
@@ -15,23 +15,52 @@ package leetcode_problems.strings;
 */
 
 public class LeetCode58 {
-    public int lengthOfLastWord(String s) {
+    // Approach 1: using trim() method
+    public int lengthOfLastWordApproach1(String s) {
+        // Step 1: Skip trailing spaces
         String trimmedStr = s.trim();
-        int length = trimmedStr.length();
 
-        int count = 0;
-        while((length>0) && (trimmedStr.charAt(length-1)!=' ')){
-            count++;
+        int length = trimmedStr.length() - 1;
+        int lastWordCharCount = 0;
+
+        // Step 2: Count characters of the last word
+        while((length >= 0) && (trimmedStr.charAt(length) != ' ')){
+            lastWordCharCount++;
             length--;
         }
 
-        return count;
+        return lastWordCharCount;
+    }
+
+    // Approach 2: without trim() method
+    public int lengthOfLastWordApproach2(String s) {
+        int lastWordCharCount = 0;
+        int i = s.length() - 1;
+
+        // Step 1: Skip trailing from last spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Step 2: Count characters of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            lastWordCharCount++;
+            i--;
+        }
+
+        return lastWordCharCount;
     }
 
     static void main(){
         LeetCode58 leetcode58 = new LeetCode58();
-        System.out.println(leetcode58.lengthOfLastWord("Hello World"));
-        System.out.println(leetcode58.lengthOfLastWord(" "));
-        System.out.println(leetcode58.lengthOfLastWord("a"));
+
+        System.out.println(leetcode58.lengthOfLastWordApproach1("Hello World"));
+        System.out.println(leetcode58.lengthOfLastWordApproach2("Hello World"));
+
+        System.out.println(leetcode58.lengthOfLastWordApproach1(" "));
+        System.out.println(leetcode58.lengthOfLastWordApproach2(" "));
+
+        System.out.println(leetcode58.lengthOfLastWordApproach1("a"));
+        System.out.println(leetcode58.lengthOfLastWordApproach2("a"));
     }
 }
