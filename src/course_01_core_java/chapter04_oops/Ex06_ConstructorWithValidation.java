4eeb32a34f9cdca8d34bb6db7aecf6506d01484b
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-02 22:12:09 +0530

    Added concept from line 11 to 27 for Ex06_ConstructorWithValidation.

--- src/course_01_core_java/chapter04_oops/Ex06_ConstructorWithValidation.java
+++ src/course_01_core_java/chapter04_oops/Ex06_ConstructorWithValidation.java
@@ -6,11 +6,30 @@ package course_01_core_java.chapter04_oops;
                 accountNumber should be non-null and non-empty.
                 balance should be non-negative.
                 Print an error message if the validation fails.
+
+    Concept:
+        1. Constructor Validation (Protecting Object State)
+           A constructor is automatically invoked when an object is created.
+           Constructors help initialize object properties during object creation.
+           By putting our validation logic inside the constructor, we are creating a "gatekeeper."
+           When we execute new Account("456", -11), the constructor immediately catches the -11.
+           It prints the required error message and sets the balance to a safe default of 0.0.
+           This guarantees that no Account object can ever exist in our program with a corrupted state.
+           (Note: I removed the final keyword from our instance variables because if validation fails,
+           and we must assign a fallback value conditionally, managing final gets slightly more complex.
+           In professional Java, instead of just printing an error, developers throw an IllegalArgumentException which instantly crashes that part of the program
+           and prevents the object from being created entirely.)
+        2. The Separation of Concerns
+           -> (Getters vs. Display)In professional software development, we should separate your data logic from your display logic.
+              The Getter (getBalance()) should solely be responsible for handing the data back to whoever asked for it.
+           -> The Test Driver (your main method) or our toString() method should be responsible for formatting and printing that data to the console.
+              If we mix them up (by putting System.out.println() inside our getters), it makes our Account class useless for graphical applications (like a mobile app or website)
+              because those applications don't use the console to display text!
  */
 
 class Account {
-    private final String accountNumber;
-    private final double balance;
+    private String accountNumber;
+    private double balance;
 
     // 1. Constructor with Validation
     public Account(String accountNumber, double balance) {
