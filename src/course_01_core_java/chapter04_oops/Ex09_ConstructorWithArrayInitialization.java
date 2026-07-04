e2151ad003311ea940fe0682df89ff3ff9e573aa
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-04 21:41:43 +0530

    Implemented a Java Classroom Class with Array Initialization.

---
+++ src/course_01_core_java/chapter04_oops/Ex09_ConstructorWithArrayInitialization.java
@@ -0,0 +1,47 @@
+package course_01_core_java.chapter04_oops;
+
+import java.util.Arrays;
+
+class Classroom{
+    private final String className;
+    private final String[] students;
+
+    public Classroom(String className, String[] students) {
+        this.className = className;
+        this.students = students;
+    }
+    public String getClassName() {
+        return className;
+    }
+
+    public String[] getStudents() {
+        return students;
+    }
+
+    public String toString() {
+        return "Classroom{" + "className = " + this.getClassName() + ", students = " + Arrays.toString(this.getStudents()) + '}';
+    }
+}
+
+public class Ex09_ConstructorWithArrayInitialization {
+    static void main() {
+
+        String[] classFirstStudents = new String[3];
+        classFirstStudents[0] = "John";
+        classFirstStudents[1] = "Doe";
+        classFirstStudents[2] = "Mary";
+
+        Classroom classroom1 = new Classroom("First", classFirstStudents);
+        System.out.println(classroom1);
+
+        String[] classSecondStudents = new String[3];
+        classSecondStudents[0] = "Dev";
+        classSecondStudents[1] = "Ram";
+        classSecondStudents[2] = "Ravana";
+
+        Classroom classroom2 = new Classroom("Second", classSecondStudents);
+        System.out.println(classroom2);
+
+
+    }
+}
