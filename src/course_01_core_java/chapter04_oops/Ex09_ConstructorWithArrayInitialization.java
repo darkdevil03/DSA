430632827416830c2614b0d9339dd84045083673
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-04 22:03:22 +0530

    For Ex09_ConstructorWithArrayInitialization, steps as comment added.

--- src/course_01_core_java/chapter04_oops/Ex09_ConstructorWithArrayInitialization.java
+++ src/course_01_core_java/chapter04_oops/Ex09_ConstructorWithArrayInitialization.java
@@ -7,13 +7,17 @@ package course_01_core_java.chapter04_oops;
 import java.util.Arrays;
 
 class Classroom{
+    // 1. Encapsulation
     private final String className;
     private final String[] students;
 
+    // 2. Parameterized Constructor
     public Classroom(String className, String[] students) {
         this.className = className;
         this.students = students;
     }
+
+    // 3. Getters
     public String getClassName() {
         return className;
     }
@@ -22,6 +26,7 @@ class Classroom{
         return students;
     }
 
+    // 4. Override toString for easy printing
     public String toString() {
         return "Classroom{" + "className = " + this.getClassName() + ", students = " + Arrays.toString(this.getStudents()) + '}';
     }
@@ -29,21 +34,16 @@ class Classroom{
 
 public class Ex09_ConstructorWithArrayInitialization {
     static void main() {
-
-        String[] classFirstStudents = new String[3];
-        classFirstStudents[0] = "John";
-        classFirstStudents[1] = "Doe";
-        classFirstStudents[2] = "Mary";
-
+        System.out.println("--- Classroom 1 ---");
+        // Optimized Array Initialization (Array Literal)
+        String[] classFirstStudents = {"John","Doe","Mary"};
         Classroom classroom1 = new Classroom("First", classFirstStudents);
         System.out.println(classroom1);
 
-        String[] classSecondStudents = new String[3];
-        classSecondStudents[0] = "Dev";
-        classSecondStudents[1] = "Ram";
-        classSecondStudents[2] = "Ravana";
-
-        Classroom classroom2 = new Classroom("Second", classSecondStudents);
+        System.out.println("\n--- Classroom 2 ---");
+        // Advanced Optimization: Anonymous Array
+        // We can create and pass the array directly inside the constructor call!
+        Classroom classroom2 = new Classroom("Second", new String[]{"Ram", "Devi", "Laxman"});
         System.out.println(classroom2);
 
 
