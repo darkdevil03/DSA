de6fabb338611523206b1cff36ae789abe155973
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-07 19:39:03 +0530

    For Ex03_StaticMethods, Added question tag.

--- src/course_01_core_java/chapter04_oops/concept02_static/Ex03_StaticMethods.java
+++ src/course_01_core_java/chapter04_oops/concept02_static/Ex03_StaticMethods.java
@@ -1,5 +1,9 @@
 package course_01_core_java.chapter04_oops.concept02_static;
 
+/*
+    Question: Write a Java program to create a class called "MathUtility" with a static method add that takes two integers and returns their sum.
+              Demonstrate the usage of this static method in the main method without creating an instance of "MathUtility".
+*/
 
 class MathUtility{
     public static int sum(int a, int b){
