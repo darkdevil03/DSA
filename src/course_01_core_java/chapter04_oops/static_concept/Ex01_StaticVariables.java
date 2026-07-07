8d4c4e229525ae7f371a350427eab1192c601e5a
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-07 17:59:03 +0530

    For Ex01_StaticVariables, Question tag added from line 4 to 6.

--- src/course_01_core_java/chapter04_oops/static_concept/Ex01_StaticVariables.java
+++ src/course_01_core_java/chapter04_oops/static_concept/Ex01_StaticVariables.java
@@ -1,5 +1,11 @@
 package course_01_core_java.chapter04_oops.static_concept;
 
+/*
+    Question: Write a Java program to create a class called "Counter" with a static variable count.
+              Implement a constructor that increments count every time an object is created.
+              Print the value of count after creating several objects.
+*/
+
 
 class Counter{
     private static int counter = 0;
