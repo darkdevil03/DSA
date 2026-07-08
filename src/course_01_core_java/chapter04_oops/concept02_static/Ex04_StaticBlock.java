c09884380a3c892f13ccc0c463633bd8935142c6
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-08 21:44:29 +0530

    For Ex04_StaticBlock, Question tag added.

--- src/course_01_core_java/chapter04_oops/concept02_static/Ex04_StaticBlock.java
+++ src/course_01_core_java/chapter04_oops/concept02_static/Ex04_StaticBlock.java
@@ -1,5 +1,9 @@
 package course_01_core_java.chapter04_oops.concept02_static;
 
+/*
+    Question: Write a Java program to create a class called "Initializer" with a static block that initializes a static variable 'initialValue' to 1000.
+              Print the value of 'initialValue' before and after creating an instance of "Initializer".
+*/
 
 class Initializer{
     static int initialValue;
