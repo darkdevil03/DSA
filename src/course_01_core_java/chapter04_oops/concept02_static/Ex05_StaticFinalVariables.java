028cf35e96e2871cdab8ab1d3569b351868e0e5d
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-07-09 21:47:09 +0530

    For Ex05_StaticFinalVariables, Added question tag.

--- src/course_01_core_java/chapter04_oops/concept02_static/Ex05_StaticFinalVariables.java
+++ src/course_01_core_java/chapter04_oops/concept02_static/Ex05_StaticFinalVariables.java
@@ -1,6 +1,10 @@
 package course_01_core_java.chapter04_oops.concept02_static;
 
-
+/*
+    Write a Java program to create a class called Constants with a static final variable 'PI' initialized to 3.14159.
+    Create a method to calculate the area of a circle given its radius, using the 'PI' constant.
+    Demonstrate the method in the main method.
+*/
 
 class Constants{
 
